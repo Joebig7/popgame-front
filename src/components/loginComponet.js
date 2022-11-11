febd4a57348febddf892ebc1dae34ff23e0400dd
JoeBig7 <1479762535@qq.com>
2022-11-11 17:54:23 +0800

    :sparkles: feat:update login page

--- src/components/loginComponet.js
+++ src/components/loginComponet.js
@@ -20,12 +20,12 @@ function LoginComponent() {
   return (
     <div className="relative basis-3/4 place-self-center">
       <form className="flex flex-col">
-        <h1 className="mb-10 text-3xl font-bold">欢迎登录Popgame</h1>
+        <h1 className="mb-16 text-4xl font-bold">欢迎登录Popgame</h1>
 
         <div className="flex flex-row ">
-          <AiOutlineUser className="pointer-events-none absolute right-3 top-[86px] order-2 fill-grey-300 text-2xl" />
+          <AiOutlineUser className="pointer-events-none absolute right-3 top-[114px] order-2 fill-grey-300 text-2xl" />
           <input
-            className="mb-10 h-12 w-full rounded-md"
+            className="mb-8 h-12 w-full rounded-lg"
             value={emailValue.email}
             type="text"
             placeholder="请输入用户名或邮箱"
@@ -36,24 +36,24 @@ function LoginComponent() {
           <AiFillEye
             hidden={passValue.showPassword}
             onClick={toggleShow}
-            className="absolute right-3 top-[178px] order-2 fill-grey-300 text-2xl"
+            className="absolute right-3 top-[196px] order-2 fill-grey-300 text-2xl"
           />
           <AiFillEyeInvisible
             hidden={!passValue.showPassword}
             onClick={toggleShow}
-            className="absolute right-3 top-[178px] order-2  fill-grey-300 text-2xl"
+            className="absolute right-3 top-[196px] order-2  fill-grey-300 text-2xl"
           />
           <input
-            className="mb-2 h-12 w-full rounded-md"
+            className="mb-6 h-12 w-full rounded-lg"
             type={passValue.showPassword ? "text" : "password"}
             placeholder="请输入密码"
           />
         </div>
 
         <Form action="/forget">
-          <button className="mb-10 font-black text-blue-700">忘记密码？</button>
+          <button className="mb-14 font-black text-blue-700">忘记密码？</button>
         </Form>
-        <button className="h-12  w-[28rem] rounded-md bg-blue-600 text-lg font-black text-white">
+        <button className="h-11  w-[28rem] rounded-md bg-blue-600 text-lg font-black text-white">
           登录
         </button>
       </form>
