12c9905f3dd3d3f5bae1ebb501858d3cc4138366
Joebig7 <1479762535@qq.com>
2022-12-13 20:00:19 +0800

    :sparkles: feat:rm title

--- src/components/login/loginFormWrapper.js
+++ src/components/login/loginFormWrapper.js
@@ -3,9 +3,6 @@ import LoginForm from "./loginForm";
 function LoginFormWrapper() {
   return (
     <div className="relative  flex h-5/6  w-full flex-col  place-items-center gap-20">
-      <div className="h-1/12 invisible relative top-56 flex  w-full  flex-row  place-content-center text-4xl font-extrabold text-base-content 3xl:visible">
-        <span className="w-1/3">欢迎来到PopGame社区</span>
-      </div>
       <LoginForm />
     </div>
   );
