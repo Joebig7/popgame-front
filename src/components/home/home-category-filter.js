569346e006100f193325a42497b5f71945aa356a
JoeBig7 <1479762535@qq.com>
2023-01-06 18:07:57 +0800

    :sparkles: add home game list

--- src/components/home/home-category-filter.js
+++ src/components/home/home-category-filter.js
@@ -1,149 +1,48 @@
-import { Fragment, useState } from "react";
-import { Dialog, Disclosure, Transition } from "@headlessui/react";
-import { XIcon } from "@heroicons/react/outline";
-import { ChevronDownIcon, PlusSmIcon } from "@heroicons/react/solid";
+import { useState, useEffect } from "react";
 import HomeGameList from "./home-game-list";
-
-const filters = [
-  {
-    id: "recommend_level",
-    name: "推荐指数",
-    options: [
-      { value: "1", label: "多半差评" },
-      { value: "2", label: "口碑一般" },
-      { value: "3", label: "多半好评" },
-      { value: "4", label: "好评如潮" },
-      { value: "5", label: "特别好评" },
-    ],
-  },
-  {
-    id: "tag",
-    name: "游戏标签",
-    options: [
-      { value: "第一人称", label: "第一人称" },
-      { value: "角色扮演", label: "角色扮演" },
-      { value: "动作", label: "动作" },
-      { value: "开放世界", label: "开放世界" },
-      { value: "回合制战斗", label: "回合制战斗" },
-      { value: "策略", label: "策略" },
-      { value: "冒险", label: "冒险" },
-      { value: "第三人称", label: "第三人称" },
-      { value: "类魂系列", label: "类魂系列" },
-    ],
-  },
-];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+import axios from "axios";
+import { POPIDEA_URL } from "../common/url-manage";
 
 export default function HomeCategoryFilter() {
-  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
+  const [gameList, setGameList] = useState([]);
+
+  const [queryParam, setQueryParam] = useState({
+    page_num: 1,
+    page_size: 20,
+    name: null,
+  });
+
+  useEffect(() => {
+    axios
+      .post(POPIDEA_URL + "/v1/games/search", {
+        page_num: queryParam.page_num,
+        page_size: queryParam.page_size,
+        name: queryParam.name
+      })
+      .then(function (response) {
+        if (response.status === 200 && response.data.code === 200) {
+          console.log(JSON.stringify(response.data.data.list))
+          setGameList(response.data.data.list);
+        } else {
+        }
+      })
+      .catch(function (error) {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
+  }, [queryParam]);
 
   return (
     <div className="bg-white">
       <div>
-        {/* Mobile filter dialog */}
-        <Transition.Root show={mobileFiltersOpen} as={Fragment}>
-          <Dialog
-            as="div"
-            className="fixed inset-0 z-40 flex lg:hidden"
-            onClose={setMobileFiltersOpen}
-          >
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-25" />
-            </Transition.Child>
-
-            <Transition.Child
-              as={Fragment}
-              enter="transition ease-in-out duration-300 transform"
-              enterFrom="translate-x-full"
-              enterTo="translate-x-0"
-              leave="transition ease-in-out duration-300 transform"
-              leaveFrom="translate-x-0"
-              leaveTo="translate-x-full"
-            >
-              <div className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
-                <div className="flex items-center justify-between px-4">
-                  <h2 className="text-lg font-medium text-gray-900">Filters</h2>
-                  <button
-                    type="button"
-                    className="-mr-2 flex h-10 w-10 items-center justify-center p-2 text-gray-400 hover:text-gray-500"
-                    onClick={() => setMobileFiltersOpen(false)}
-                  >
-                    <span className="sr-only">Close menu</span>
-                    <XIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
-                </div>
-
-                {/* Filters */}
-                <form className="mt-4">
-                  {filters.map((section) => (
-                    <Disclosure
-                      as="div"
-                      key={section.name}
-                      className="border-t border-gray-200 pt-4 pb-4"
-                    >
-                      {({ open }) => (
-                        <fieldset>
-                          <legend className="w-full px-2">
-                            <Disclosure.Button className="flex w-full items-center justify-between p-2 text-gray-400 hover:text-gray-500">
-                              <span className="text-sm font-medium text-gray-900">
-                                {section.name}
-                              </span>
-                              <span className="ml-6 flex h-7 items-center">
-                                <ChevronDownIcon
-                                  className={classNames(
-                                    open ? "-rotate-180" : "rotate-0",
-                                    "h-5 w-5 transform"
-                                  )}
-                                  aria-hidden="true"
-                                />
-                              </span>
-                            </Disclosure.Button>
-                          </legend>
-                          <Disclosure.Panel className="px-4 pt-4 pb-2">
-                            <div className="space-y-6">
-                              {section.options.map((option, optionIdx) => (
-                                <div
-                                  key={option.value}
-                                  className="flex items-center"
-                                >
-                                  <input
-                                    id={`${section.id}-${optionIdx}-mobile`}
-                                    name={`${section.id}[]`}
-                                    defaultValue={option.value}
-                                    type="checkbox"
-                                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                  />
-                                  <label
-                                    htmlFor={`${section.id}-${optionIdx}-mobile`}
-                                    className="ml-3 text-sm text-gray-500"
-                                  >
-                                    {option.label}
-                                  </label>
-                                </div>
-                              ))}
-                            </div>
-                          </Disclosure.Panel>
-                        </fieldset>
-                      )}
-                    </Disclosure>
-                  ))}
-                </form>
-              </div>
-            </Transition.Child>
-          </Dialog>
-        </Transition.Root>
-
         <main className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
           <div className="border-b border-gray-200 pb-10">
             <h1 className="text-4xl font-extrabold tracking-tight text-gray-900">
@@ -155,66 +54,9 @@ export default function HomeCategoryFilter() {
           </div>
 
           <div className="pt-12 lg:grid lg:grid-cols-3 lg:gap-x-8 xl:grid-cols-4">
-            <aside>
-              <h2 className="sr-only">Filters</h2>
-
-              <button
-                type="button"
-                className="inline-flex items-center lg:hidden"
-                onClick={() => setMobileFiltersOpen(true)}
-              >
-                <span className="text-sm font-medium text-gray-700">
-                  Filters
-                </span>
-                <PlusSmIcon
-                  className="ml-1 h-5 w-5 flex-shrink-0 text-gray-400"
-                  aria-hidden="true"
-                />
-              </button>
-
-              <div className="hidden lg:block">
-                <form className="space-y-10 divide-y divide-gray-200">
-                  {filters.map((section, sectionIdx) => (
-                    <div
-                      key={section.name}
-                      className={sectionIdx === 0 ? null : "pt-10"}
-                    >
-                      <fieldset>
-                        <legend className="block text-sm font-medium text-gray-900">
-                          {section.name}
-                        </legend>
-                        <div className="space-y-3 pt-6">
-                          {section.options.map((option, optionIdx) => (
-                            <div
-                              key={option.value}
-                              className="flex items-center"
-                            >
-                              <input
-                                id={`${section.id}-${optionIdx}`}
-                                name={`${section.id}[]`}
-                                defaultValue={option.value}
-                                type="checkbox"
-                                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                              />
-                              <label
-                                htmlFor={`${section.id}-${optionIdx}`}
-                                className="ml-3 text-sm text-gray-600"
-                              >
-                                {option.label}
-                              </label>
-                            </div>
-                          ))}
-                        </div>
-                      </fieldset>
-                    </div>
-                  ))}
-                </form>
-              </div>
-            </aside>
-
             {/* Product grid */}
-            <div className="mt-6 lg:col-span-2 lg:mt-0 xl:col-span-3">
-              <HomeGameList />
+            <div className="mt-6 lg:col-span-2 lg:mt-0 xl:col-span-4">
+              <HomeGameList gameList={gameList} />
             </div>
           </div>
         </main>
--- src/components/home/home-game-list.js
+++ src/components/home/home-game-list.js
@@ -1,41 +1,51 @@
-const files = [
-  {
-    title: "IMG_4985.HEIC",
-    size: "3.9 MB",
-    source:
-      "https://cdn.cloudflare.steamstatic.com/steam/apps/501300/header.jpg",
-  },
-];
-
-export default function HomeGameList() {
+/* eslint-disable eqeqeq */
+export default function HomeGameList({ gameList }) {
   return (
-    <ul
-      role="list"
-      className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8"
-    >
-      {files.map((file) => (
-        <li key={file.source} className="relative">
-          <div className="aspect-w-12 aspect-h-7 group block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-            <img
-              src={file.source}
-              alt=""
-              className="pointer-events-none object-cover group-hover:opacity-75"
-            />
-            <button
-              type="button"
-              className="absolute inset-0 focus:outline-none"
-            >
-              <span className="sr-only">View details for {file.title}</span>
-            </button>
-          </div>
-          <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
-            {file.title}
-          </p>
-          <p className="pointer-events-none block text-sm font-medium text-gray-500">
-            {file.size}
-          </p>
-        </li>
-      ))}
-    </ul>
+    <>
+      {console.log("xxxx" + JSON.stringify(gameList))}
+
+      <ul
+        role="list"
+        className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-1 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8"
+      >
+        {gameList.map((game) => (
+          <li key={game.id} className="relative">
+            <div className="aspect-w-12 aspect-h-7 group block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+              <img
+                src={game.cover_url}
+                alt=""
+                className="pointer-events-none object-cover group-hover:opacity-75"
+              />
+              <button
+                type="button"
+                className="absolute inset-0 focus:outline-none"
+              >
+                <span className="sr-only"> {game.name_cn}</span>
+              </button>
+            </div>
+            <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
+              {game.name_cn}
+            </p>
+            <p className="pointer-events-none block text-sm font-medium text-gray-500">
+              {getRecommendLevelDes(game.recommend_level)}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </>
   );
+}
+
+function getRecommendLevelDes(level) {
+  if (level == 1) {
+    return "多半差评";
+  } else if (level == 2) {
+    return "整体一般";
+  } else if (level == 3) {
+    return "多半好评";
+  } else if (level == 4) {
+    return "好评如潮";
+  } else if (level == 5) {
+    return "特别好评";
+  }
 }
